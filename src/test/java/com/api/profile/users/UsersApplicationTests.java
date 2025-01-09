2621a4c558c6602fdad3ef8af0479d53a33d5e90
Kamishiro2003 <darwingranados96@gmail.com>
2025-01-09 09:21:16 -0500

    upload project

---
+++ src/test/java/com/api/profile/users/UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.profile.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
